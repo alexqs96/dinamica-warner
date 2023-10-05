74a6274a23f3986d7e01e137361ac725723795bb
AgustinC91 <nahuel.cordoba91@gmail.com>
2023-10-05 20:29:10 -0300

    home

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import { Button_Start } from "@/components/Button/Button_start";
 import { StatsBar } from "@/components/Chart";
+import { Qr } from "@/components/Qr/Qr";
 
 export default function Home() {
   return (
     <>
-    <StatsBar />
+        <Qr/>
     </>
   )
 }
