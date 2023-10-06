3e719af8f53fff21ff24d5e73634513410cef8f1
alexqs96 <alexander.mamani.dev@gmail.com>
2023-10-06 00:30:37 -0300

    fix

--- src/app/vota/page.js
+++ src/app/vota/page.js
@@ -1,11 +1,13 @@
 import Memes from "../../components/memes";
 
 export default async function VotePage(){
-  let data = []
+  let data = {}
   try {
-    data = await fetch(`${process.env.BASE_URL}/api/list`).then(res => res.json())
+    data = await fetch(`${process.env.BASE_URL}/api/list`, {
+      cache: "no-store"
+    }).then(res => res.json())
   } catch (error) {
-    data = []
+    data = {}
   }
 
   return (
