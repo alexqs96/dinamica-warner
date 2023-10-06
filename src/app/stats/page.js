7ac585673e2e127d4bc852d1834a3602d97a10b7
alexqs96 <alexander.mamani.dev@gmail.com>
2023-10-06 00:23:36 -0300

    fix

--- src/app/stats/page.js
+++ src/app/stats/page.js
@@ -2,9 +2,15 @@ import { StatsBar } from '@/components/Chart'
 import "./stats.css"
 
 const Stats = async () => {
-  const data = await fetch(`${process.env.BASE_URL}/api/votes`,{
+  let data = []
+  
+  try {
+    data = await fetch(`${process.env.BASE_URL}/api/votes`,{
       cache: "no-store"
-    }).then(res => res.json())
+    }).then(res => res.json()) 
+  } catch (error) {
+    data = []
+  }
 
   return (
     <section className="container_stats">
